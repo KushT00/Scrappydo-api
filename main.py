c757145fc3ace90cb6e576d39668552b51d04902
KushT00 <136300357+KushT00@users.noreply.github.com>
2025-04-05 17:56:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,12 +59,12 @@ def scrape_website(url):
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-
+        options.binary_location = "/usr/bin/chromium"
+        
         # Automatically install and use the correct ChromeDriver
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
+        driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+
 
         driver.get(url)
         time.sleep(3)  # Allow some time for JavaScript to load
